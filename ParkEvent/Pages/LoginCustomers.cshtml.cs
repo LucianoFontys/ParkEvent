16864e4dcd3e4fdd29f881b13ac59f2b3dae6450
Luciano <l.llera@student.fontys.com>
2025-06-11 15:30:48 +0200

    Added pages and CSS. Event overview and login pages.

---
+++ ParkEvent/Pages/LoginCustomers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ParkEvent.Pages
+{
+    public class LoginCustomersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ParkEvent/Pages/LoginEmployees.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ParkEvent.Pages
+{
+    public class LoginEmployeesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
